c6e6c16eb1db7096215d645df8ec1737de70be5a
Tapani Savo <202488780+tapanisavo@users.noreply.github.com>
2025-03-15 11:18:20 +0200

    Testing environment variables

--- api/src/functions/processImage.js
+++ api/src/functions/processImage.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const { app } = require('@azure/functions');
 
 app.http('processImage', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, __) => {
-        return {jsonBody: { text: "Hello, world!"}};
+        return {jsonBody: { text: process.env.MICROSOFT_VISION_AI_ENDPOINT + ' :: ' + process.env.MICROSOFT_VISION_AI_SECRET }};
     }
 });
