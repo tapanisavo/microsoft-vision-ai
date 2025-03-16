7ac0bd2625203bfed8e0cf68047e6593c65d1ec8
Tapani Savo <202488780+tapanisavo@users.noreply.github.com>
2025-03-16 15:37:22 +0200

    Disabling the function until I get it to work

--- api/src/functions/processImage.js
+++ api/src/functions/processImage.js
@@ -14,31 +14,31 @@ app.http('processImage', {
 });
 
 async function processImage(imageUrl) {
-    const client = DocumentIntelligence(endpoint, {key: key});
-    const initialResponse = await client
-        .path("/documentModels/{modelId}:analyze", "prebuilt-receipt")
-        .post({
-        contentType: "application/json",
-        body: {
-            base64Source: imageUrl
-        }
-    });
+    // const client = DocumentIntelligence(endpoint, {key: key});
+    // const initialResponse = await client
+    //     .path("/documentModels/{modelId}:analyze", "prebuilt-receipt")
+    //     .post({
+    //     contentType: "application/json",
+    //     body: {
+    //         base64Source: imageUrl
+    //     }
+    // });
 
-    if (isUnexpected(initialResponse)) {
-        return initialResponse.body.error;
-    }
+    // if (isUnexpected(initialResponse)) {
+    //     return initialResponse.body.error;
+    // }
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    const poller = await getLongRunningPoller(client, initialResponse);
-    // Some hack to check if the poller has the polling function, 
-    // since sometimes getLongRunningPoller returns the actual results instead of a poller
-    if (Object.prototype.hasOwnProperty.call(poller, 'pollUntilDone')) {
-        return (await poller.pollUntilDone()).body;
-    }
-    else if (Object.prototype.hasOwnProperty.call(poller, 'body')) {
-        return poller.body;
-    }
+    // const poller = await getLongRunningPoller(client, initialResponse);
+    // // Some hack to check if the poller has the polling function, 
+    // // since sometimes getLongRunningPoller returns the actual results instead of a poller
+    // if (Object.prototype.hasOwnProperty.call(poller, 'pollUntilDone')) {
+    //     return (await poller.pollUntilDone()).body;
+    // }
+    // else if (Object.prototype.hasOwnProperty.call(poller, 'body')) {
+    //     return poller.body;
+    // }
     
     return 'Image processing failed!';
 }
