c85f99eab3d9852994a3ba15fd604373b7581219
Tapani Savo <202488780+tapanisavo@users.noreply.github.com>
2025-03-16 15:33:24 +0200

    More build error fixes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         }
       })
     }    
-  }, [setPreviewImageUrl, setRawResponse, setTableData]);
+  });
 
   function processImage(value) {
     fetch('/api/processImage', {
