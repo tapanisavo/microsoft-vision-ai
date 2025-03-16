c701f8ab32b84b569ae885df805736b12f23fc3a
Tapani Savo <202488780+tapanisavo@users.noreply.github.com>
2025-03-16 13:03:13 +0200

    Processed the API response to a HTML table

--- api/src/functions/processImage.js
+++ api/src/functions/processImage.js
@@ -1,13 +1,44 @@
 require('dotenv').config()
+
 const { app } = require('@azure/functions');
+const DocumentIntelligence = require("@azure-rest/ai-document-intelligence").default;
+const { getLongRunningPoller, isUnexpected } = require("@azure-rest/ai-document-intelligence");
+
+const endpoint = process.env.MICROSOFT_VISION_AI_ENDPOINT;
+const key = process.env.MICROSOFT_VISION_AI_SECRET;
 
 app.http('processImage', {
     methods: ['POST'],
     authLevel: 'anonymous',
-    handler: async (request, __) => {
-        return {jsonBody: { 
-            text: process.env.MICROSOFT_VISION_AI_ENDPOINT + ' :: ' + process.env.MICROSOFT_VISION_AI_SECRET,
-            image: request.imageUrl
-        }};
-    }
+    handler: ({ imageUrl }, __) => ({jsonBody: {result: processImage(imageUrl)}})
 });
+
+async function processImage(imageUrl) {
+    const client = DocumentIntelligence(endpoint, {key: key});
+    const initialResponse = await client
+        .path("/documentModels/{modelId}:analyze", "prebuilt-receipt")
+        .post({
+        contentType: "application/json",
+        body: {
+            base64Source: imageUrl
+        }
+    });
+
+    if (isUnexpected(initialResponse)) {
+        return initialResponse.body.error;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const poller = await getLongRunningPoller(client, initialResponse);
+    // Some hack to check if the poller has the polling function, 
+    // since sometimes getLongRunningPoller returns the actual results instead of a poller
+    if (Object.prototype.hasOwnProperty.call(poller, 'pollUntilDone')) {
+        return (await poller.pollUntilDone()).body;
+    }
+    else if (Object.prototype.hasOwnProperty.call(poller, 'body')) {
+        return poller.body;
+    }
+    
+    return 'Image processing failed!';
+}
--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { useRef, useState, useEffect } from 'react';
 
 function App() {
   const ref = useRef(null);
-  const [previewImageUrl, setPreviewImageUrl] = useState('');  
+  const [previewImageUrl, setPreviewImageUrl] = useState('');
+  const [rawResponse, setRawResponse] = useState('');
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     const element = ref?.current;
 
     if (element) {
       element.addEventListener('change', () => {
+        setPreviewImageUrl('');
+        setRawResponse('');
+        setTableData([]);
+
         const file = element.files[0];
 
         if (file) {
@@ -16,19 +22,14 @@ function App() {
 
           reader.onloadend = (event) => {
             setPreviewImageUrl(event.target.result);
-            processImage(event.target.result);
+            processImage(event.target.result.split(',')[1]);
           };
 
           reader.readAsDataURL(file);
         }
-        else {
-          setPreviewImageUrl('');
-        }
       })
     }    
-  }, [setPreviewImageUrl]);
-
-  const [imageContents, setImageContents] = useState('')
+  }, [setPreviewImageUrl, setRawResponse, setTableData, processImage]);
 
   function processImage(value) {
     fetch('/api/processImage', {
@@ -37,8 +38,51 @@ function App() {
       body: {imageUrl: value}
     })
     .then(response => response.json())
-    .then(json => setImageContents(json.text))
-    .catch(() => setImageContents('Failed to read image!'));    
+    .then(json => processResults(json.result))
+    .catch((error) => {
+      console.log(error);
+      setRawResponse('Failed to read image!');
+    });    
+  }
+
+  function processResults(json) {
+    if (json.status !== "succeeded") {
+      setRawResponse('Failed to process image!');
+      return;
+    }
+
+    const tableData = [];
+
+    for(let documentIndex = 0; documentIndex < json.analyzeResult.documents.length; documentIndex++) {
+      const document = json.analyzeResult.documents[documentIndex];
+      const {fields: {Items, MerchantName, MerchantPhoneNumber, ReceiptType, Total, TransactionDate, TransactionTime}} = document;
+
+      if (ReceiptType) {
+        tableData.push(['Receipt type: ', ReceiptType.valueString])
+      }
+
+      if (TransactionDate && TransactionTime) {
+        tableData.push(['Purchase date: ', TransactionDate.valueDate + ' ' + TransactionTime.valueTime])
+      }
+
+      if (MerchantName) {
+        tableData.push(['Merchant: ', MerchantName.valueString + (MerchantPhoneNumber ? ` (${MerchantPhoneNumber.valuePhoneNumber})` : '')])
+      }
+
+      tableData.push(['Items:'])
+
+      if (Items) {
+        tableData.push(...Items.valueArray.map(({ valueObject }) => 
+          [valueObject.Description.valueString, valueObject.TotalPrice.valueCurrency.amount + ' ' + valueObject.TotalPrice.valueCurrency.currencyCode])
+        )
+      }
+
+      if (Total) {
+        tableData.push(['Total', Total.valueCurrency.amount + ' ' + Total.valueCurrency.currencyCode])
+      }
+    }
+
+    setTableData(tableData);
   }
 
   return (
@@ -49,8 +93,18 @@ function App() {
           <img src={previewImageUrl} style={{ display: 'block', height: '200px' }} alt="Preview" /> :
           null
       }
+      
+      {
+        tableData.length ?
+        <table>
+          <tbody>
+            {tableData.map(row => <tr>{row.map((column) => <td>{column}</td>)}</tr>)}
+          </tbody>
+        </table> :
+        null
+      }
 
-      <div className='response'>{imageContents}</div>
+      <div className='response'>{rawResponse}</div>
     </div>
   );
 }
